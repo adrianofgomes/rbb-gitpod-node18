bacb8cb0bc124a3626e9e78126fba7b3199562e1
adrianofgomes <adrianofgomes@gmail.com>
2023-11-20 19:36:49 +0000

    ajuste no lab1 - blocoGanache.js

--- lab1/back/blocoGanache.js
+++ lab1/back/blocoGanache.js
@@ -1,6 +1,6 @@
 const ethers = require("ethers")
 
-const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:545");
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
 
 getBloco();
 
